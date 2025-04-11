31668e42c4d40f25092f7a5e38de370962bbc483
32r3cx1 <147966209+32r3cx1@users.noreply.github.com>
2025-04-11 18:33:25 +0800

    Delete src directory

--- src/Example.java
+++
@@ -1,9 +0,0 @@
-public class Example {
-    public void process(List<String> data) {
-        for (int i = 0; i < data.size(); i++) {
-            for (int j = 0; j < data.get(i).length(); j++) {
-                System.out.println(data.get(i).charAt(j));
-            }
-        }
-    }
-}
